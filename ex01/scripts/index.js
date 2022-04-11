2431aa834629c1b045a0511705f9ad627ed987c2
Atrus1 <atrus2012@gmail.com>
2022-04-11 11:15:53 +0200

    Adding files

---
+++ ex01/scripts/index.js
@@ -0,0 +1,5 @@
+var ls_input_section_header = document.createElement("img");   
+ls_input_section_header.title="info to display on hover of info";
+ls_input_section_header.className="ls_input_section_header_gs";
+ls_input_section_header.setAttribute("src","/images/img1.jbg");
+ls_input_section.appendChild(ls_input_section_header);   
